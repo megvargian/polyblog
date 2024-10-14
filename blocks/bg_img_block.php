22cb96ef2b16b7e2ef5e2432bc81dc14c0d8778b
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-10-14 22:46:30 +0300

    add conddition example

--- blocks/bg_img_block.php
+++ blocks/bg_img_block.php
@@ -6,7 +6,11 @@
 $bg_img_block = get_fields();
 ?>
 <section class="py-3">
-    <a href="<?php echo $bg_img_block['main_image_url'] ?>">
+    <?php if($bg_img_block['main_image_url']) {?>
+        <a href="<?php echo $bg_img_block['main_image_url'] ?>">
+            <img class="w-100" src="<?php echo $bg_img_block['main_image']; ?>" alt="">
+        </a>
+    <?php } else { ?>
         <img class="w-100" src="<?php echo $bg_img_block['main_image']; ?>" alt="">
-    </a>
+    <?php } ?>
 </section>
