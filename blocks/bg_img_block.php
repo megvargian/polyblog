6949145e2f773832e1b32b08153897ab6b895591
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-10-16 01:21:15 +0300

    small fix

--- blocks/bg_img_block.php
+++ blocks/bg_img_block.php
@@ -8,9 +8,9 @@ $bg_img_block = get_fields();
 <section class="">
     <?php if($bg_img_block['main_image_url']) {?>
         <a href="<?php echo $bg_img_block['main_image_url'] ?>">
-            <img class="w-100" src="<?php echo $bg_img_block['main_image']; ?>" alt="">
+            <img class="w-100 d-block" src="<?php echo $bg_img_block['main_image']; ?>" alt="">
         </a>
     <?php } else { ?>
-        <img class="w-100" src="<?php echo $bg_img_block['main_image']; ?>" alt="">
+        <img class="w-100 d-block" src="<?php echo $bg_img_block['main_image']; ?>" alt="">
     <?php } ?>
 </section>
