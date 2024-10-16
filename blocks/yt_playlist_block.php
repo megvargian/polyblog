275be9bd58efa9c02d1ae2bf484ecc763125ad4b
Hrag Harabetian <hrag.harabetian@cri.nazionale>
2024-10-16 22:57:47 +0200

    style

--- blocks/yt_playlist_block.php
+++ blocks/yt_playlist_block.php
@@ -7,24 +7,23 @@ $yt_playlist_block = get_fields();
 ?>
 <section class="w-100 youtube-playlist-bg">
     <div class="row">
-        <div class="col">
+        <div class="col youtube-playlist-button">
             <a class="youtube_playlist_img" href="<?php echo $yt_playlist_block['title_link'] ?>" target="_blank">
                 <img src="<?php echo $yt_playlist_block['title']; ?>" alt="YouTube">
             </a>
         </div>
-        <div class="col">
+        <div class="col youtube-playlist-button">
             <a href="<?php echo $yt_playlist_block['yt_link'] ?>" target="_blank">
                 <img src="http://polybloglb.com/wp-content/uploads/2024/10/clickformore.png" alt="YouTube">
             </a>
         </div>
-        <div class="col">
+        <div class="col youtube-playlist-button">
             <img src="<?php echo $yt_playlist_block['yt_image']; ?>" alt="YouTube">
         </div>
-        <div class="col">
+        <div class="col youtube-playlist-button">
             <a class="youtube_playlist_img" href="<?php echo $yt_playlist_block['episodes_link'] ?>" target="_blank">
                 <img src="<?php echo $yt_playlist_block['episodes']; ?>" alt="YouTube">
             </a>
         </div>
     </div>
 </section>
-<p>مجل&#1600;&#1600;&#1600;&#1600;&#1600;&#1600;&#1600;&#1600;س &#1600;&#1600; المستشارين</p>
