a1b85174572c00f75d372cf5eac0a2fa14f29fe6
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-10-18 22:54:53 +0300

    add contact us block

--- blocks/contactus_block.php
+++ blocks/contactus_block.php
@@ -8,8 +8,8 @@ $contactus_fields = get_fields();
 <section class="py-4 contact-us-section">
     <div class="container-fluid">
         <form action="">
-            <div class="row">
-                <div class="col-5">
+            <div class="row justify-content-center">
+                <div class="col-4">
                     <input type="text" placeholder="your Name" required>
                     <input type="email" placeholder="email Address" required>
                     <div class="row px-0">
@@ -24,10 +24,10 @@ $contactus_fields = get_fields();
                     <input type="text" placeholder="areas of interest" required>
                     <input type="text" placeholder="Links to previous writings if present" required>
                 </div>
-                <div class="col-5 custom-text-size-for-placeholder">
-                    <input class="h-100" type="text" placeholder="Write \r\n your  \r\n pitch  \r\n here">
+                <div class="col-6 custom-text-size-for-placeholder">
+                    <input class="h-100" type="text" placeholder="Write your pitch here">
                 </div>
-                <div class="col-2">
+                <div class="col-1">
                     <button type="submit">
                         <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/" alt="submit-icon">
                     </button>
