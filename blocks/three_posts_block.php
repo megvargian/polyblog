707eee5ac83ad2b0e875462887f2886c7d50e357
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-10-20 22:27:07 +0300

    add three posts block

--- blocks/three_posts_block.php
+++ blocks/three_posts_block.php
@@ -8,17 +8,17 @@ $three_posts_fields = get_fields();
 <section class="py-0">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-4">
+            <div class="col-4 px-0">
                 <a href="#" class="d-block">
                     <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/matha_na3lam.png" alt="matha_na3lam" class="d-block w-100">
                 </a>
             </div>
-            <div class="col-4">
+            <div class="col-4 px-0">
                 <a href="#" class="d-block">
                     <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/matha_na3lam2.png" alt="matha_na3lam" class="d-block w-100">
                 </a>
             </div>
-            <div class="col-4">
+            <div class="col-4 px-0">
                 <a href="#" class="d-block">
                     <img src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/matha_na3lam.png" alt="matha_na3lam" class="d-block w-100">
                 </a>
