9d8e5ee4373548a96baf7d482582c87e0ab2e7d5
hrag <hrag.h@hotmail.com>
2024-10-20 13:55:45 +0200

    typos

--- blocks/single_featured_article_block.php
+++ blocks/single_featured_article_block.php
@@ -9,8 +9,8 @@ $title = get_the_title($featured_article_block);
 $article_link = get_permalink($featured_article_block);
 $tags = get_the_tags($featured_article_block);
 $categories = get_the_category($featured_article_block);
-$author = get_field('author_field', $featured_article_block);
-$thumbnail = get_field('thumbnail_field', $featured_article_block);
+$author = get_field('author', $featured_article_block);
+$thumbnail = get_field('article_thumbnail', $featured_article_block);
 ?>
 <div class="single-featured-article-container">
     <img src="<?php echo $thumbnail; ?>" alt="<?php echo $title; ?>" />
