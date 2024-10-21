f517176950bda730609ee2ea06da8b678821b428
hrag <hrag.h@hotmail.com>
2024-10-21 17:44:31 +0200

    fix

--- blocks/special_productions_block.php
+++ blocks/special_productions_block.php
@@ -8,8 +8,8 @@ $special_productions_block = get_fields();
 
 <div class="container-fluid special-productions">
     <div class="row">
-        <div class="col">
-            <a href="<?php echo $special_productions_block['special_productions_url'] ?>">
+        <div class="col px-0">
+            <a href="<?php echo $special_productions_block['special_productions_url'] ?>" target="_blank">
                 <img class="w-100 d-block" src="<?php echo $special_productions_block['special_productions_image']; ?>" alt="Special productions">
             </a>
         </div>
