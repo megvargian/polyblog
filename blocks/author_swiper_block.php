59c0255aae61af2f054e606a055a93839d7c1466
hrag <hrag.h@hotmail.com>
2024-10-22 15:33:10 +0200

    swiper

--- blocks/author_swiper_block.php
+++ blocks/author_swiper_block.php
@@ -4,23 +4,32 @@
  */
 
 $author_swiper_block = get_fields();
-
-// $title = get_the_title($author_swiper_block);
-// $author_image = $author_swiper_block['author_profile'];
 $authors_list = $author_swiper_block['authors_list'];
 ?>
 <div class="container-fluid">
-    <pre><?php print_r($authors_list); ?></pre>
-    <?php foreach ($authors_list as $author) { ?>
-        <div class="row">
-            <div class="col">
-                <?php $author_id = $author['author'];
-                $author_title = get_the_title($author_id);
-                $author_image = get_field('author_profile', $author_id); ?>
 
-                <img src="<?php echo $author_image ?>" />
-                <p><?php echo esc_html($author_title); ?></p>
+    <div class="row">
+        <div class="col">
+            <div class="swiper">
+                <div class="swiper-wrapper">
+                    <?php foreach ($authors_list as $author) { ?>
+                        <div class="swiper-slide">
+                            <?php $author_id = $author['author'];
+                            $author_title = get_the_title($author_id);
+                            $author_image = get_field('author_profile', $author_id); ?>
+                            <!-- <p><?php echo esc_html($author_title); ?></p> -->
+                            <img src="<?php echo $author_image ?>" alt="<?php echo $author_title ?>" />
+                        </div>
+                    <?php } ?>
+                </div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
             </div>
+
+
+
+
         </div>
-    <?php } ?>
+    </div>
+
 </div>
