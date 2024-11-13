87ac7e39d779461ae399a8d139ce3fee255453a1
hrag <hrag.h@hotmail.com>
2024-11-13 16:49:10 +0100

    single-post.php

--- single-post.php
+++ single-post.php
@@ -26,10 +26,16 @@ if (have_posts()):
                 </div>
             </div>
             <div class="row tags">
-                <div class="col">
-                    <?php foreach ($tags as $tag) { ?>
-                        <?php echo "#" . esc_html($tag->name); ?>
-                    <?php } ?>
+                <div class="col-8">
+                    <?php
+                    echo '<p>Published on: ' . get_the_date('d/m/Y') . ' ' . get_the_time('g:i A') . '</p>';
+                    ?>
+                </div>
+                <div class="col-4">
+                    <p> <?php foreach ($tags as $tag) { ?>
+                            <?php echo "#" . esc_html($tag->name); ?>
+                        <?php } ?>
+                    </p>
                 </div>
             </div>
             <div class="row">
