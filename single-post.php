b4794a1a6798b3e0705fedb9dece8315041b71c7
hrag <hrag.h@hotmail.com>
2024-11-13 15:23:59 +0100

    single-post.php

--- single-post.php
+++ single-post.php
@@ -12,12 +12,7 @@ get_header(); ?>
     <?php
     while (have_posts()):
         the_post();
-
-        get_template_part('template-parts/content', get_post_format());
-
-        the_post_navigation();
-
-    endwhile; // End of the loop.
+    endwhile;
     ?>
     <div>
         testing single post type
