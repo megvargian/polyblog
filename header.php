00fd41c72ac945fa81adaa1086b9b5d23c8ac677
hrag <hrag.h@hotmail.com>
2024-11-13 15:53:01 +0100

    header.php

--- header.php
+++ header.php
@@ -37,15 +37,13 @@ $header_video_url = get_field('header_video', 'option');
 						src="http://polybloglb.com/wp-content/uploads/2024/10/politics_not_news.png"
 						alt="Politics, not news.">
 				</a>
-				<?php
-				if (is_front_page()) {
-					?>
+				<?php if (is_front_page()) { ?>
 					<div class="video-container">
 						<video class="video" width="100%" height="100%" autoplay loop muted>
 							<source src="<?php echo esc_url($header_video_url); ?>" type="video/mp4">
 							Your browser does not support the video tag.
 						</video>
-						<div class="row header-button-container">
+						<div class="row header-button-container-with-video">
 							<div class="col">
 								<a href="http://polybloglb.com/" target="_blank">
 									<img class="w-100 d-block"
@@ -83,7 +81,43 @@ $header_video_url = get_field('header_video', 'option');
 							</div>
 						</div>
 					</div>
-					<?php
-				}
-				?>
+				<?php } else { ?>
+					<div class="row header-button-container-with-video">
+						<div class="col">
+							<a href="http://polybloglb.com/" target="_blank">
+								<img class="w-100 d-block"
+									src="http://polybloglb.com/wp-content/uploads/2024/10/what_we_think.png"
+									alt="What we think">
+							</a>
+						</div>
+						<div class="col">
+							<a href="http://polybloglb.com/" target="_blank">
+								<img class="w-100 d-block"
+									src="http://polybloglb.com/wp-content/uploads/2024/10/how_we_see_it.png"
+									alt="How we see it">
+							</a>
+						</div>
+						<div class="col">
+							<a href="http://polybloglb.com/" target="_blank">
+								<img class="w-100 d-block"
+									src="http://polybloglb.com/wp-content/uploads/2024/10/our_segments.png"
+									alt="Our segments">
+							</a>
+						</div>
+						<div class="col">
+							<a href="http://polybloglb.com/" target="_blank">
+								<img class="w-100 d-block"
+									src="http://polybloglb.com/wp-content/uploads/2024/10/out_productions.png"
+									alt="Our productions">
+							</a>
+						</div>
+						<div class="col">
+							<a href="http://polybloglb.com/" target="_blank">
+								<img class="w-100 d-block"
+									src="http://polybloglb.com/wp-content/uploads/2024/10/get_to_know_us.png"
+									alt="Get to know us">
+							</a>
+						</div>
+					</div>
+				<?php } ?>
 			</div>
