26ef121dde53e1904969822eb0ef3dde98b39ea3
hrag <hrag.h@hotmail.com>
2024-11-28 18:33:12 +0100

    homepage author block

--- blocks/author_block.php
+++ blocks/author_block.php
@@ -10,13 +10,13 @@ $rows = array_chunk($authors_list, 4);
 <div class="container-fluid author-block-container">
     <?php foreach ($rows as $row) { ?>
         <div class="row">
-            <div class="col">
-                <?php foreach ($row as $author) { ?>
-                    <?php $author_id = $author['author'];
-                    $author_title = get_the_title($author_id);
-                    $author_image = get_field('author_profile', $author_id);
-                    $author_expertise = get_field('expertise', $author_id);
-                    $author_link = get_permalink($author_id); ?>
+            <?php foreach ($row as $author) { ?>
+                <?php $author_id = $author['author'];
+                $author_title = get_the_title($author_id);
+                $author_image = get_field('author_profile', $author_id);
+                $author_expertise = get_field('expertise', $author_id);
+                $author_link = get_permalink($author_id); ?>
+                <div class="col">
                     <div class="single-author-container">
                         <a href="<?php echo $author_link; ?>" target="_blank">
                             <img class="author-image" src="<?php echo $author_image ?>" alt="<?php echo $author_title ?>" />
@@ -24,8 +24,8 @@ $rows = array_chunk($authors_list, 4);
                         <p><?php echo esc_html($author_title); ?></p>
                         <p><?php echo esc_html($author_expertise) ?></p>
                     </div>
-                <?php } ?>
-            </div>
+                </div>
+            <?php } ?>
         </div>
     <?php } ?>
 </div>
