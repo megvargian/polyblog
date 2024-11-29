47d50959147854ff7732401c8f8a6460fe7471cf
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-11-29 17:51:49 +0200

    fix message from our writer block

--- blocks/message_from_editor_block.php
+++ blocks/message_from_editor_block.php
@@ -9,15 +9,15 @@ $message_from_deitor_fields = get_fields();
     <img class="w-100 d-block" src="<?php echo $message_from_deitor_fields['main_image']; ?>" alt="message_from_directo_bannerr">
     <div class="container-fluid py-4 px-4">
         <div class="row">
-            <div class="col-4">
+            <div class="col">
                <p class="en">
                     <?php echo $message_from_deitor_fields['en_text']; ?>
                </p>
             </div>
-            <div class="col-4 justify-content-center d-flex">
+            <div class="col-3 justify-content-center d-flex">
                 <img src="<?php echo $message_from_deitor_fields['middle_profile_image'];?>" alt="profile">
             </div>
-            <div class="col-4">
+            <div class="col">
                <p class="ar">
                     <?php echo $message_from_deitor_fields['ar_text']; ?>
                </p>
