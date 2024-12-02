385bc06382075b4e133dad7bbbb95f033b84056c
hrag <hrag.h@hotmail.com>
2024-12-02 17:17:18 +0100

    3 featured articles block

---
+++ blocks/three_featured_articles_block.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * HomePage Three Featured Articles Block Template
+ */
+
+$three_articles_block = get_fields();
+?>
+<section class="three-featured-articles-block-container">
+    <div class="container">
+        <div class="row">
+            <div class="col">1</div>
+            <div class="col">2</div>
+            <div class="col">3</div>
+        </div>
+    </div>
+</section>
