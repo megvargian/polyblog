a1c9aa0a928e80218c4e41d7596a9f28b67915d7
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-12-02 17:46:33 +0200

    fix swiper

--- blocks/author_block.php
+++ blocks/author_block.php
@@ -7,25 +7,29 @@ $author_block = get_fields();
 $authors_list = $author_block['authors_list'];
 $rows = array_chunk($authors_list, 4);
 ?>
-<div class="container-fluid author-block-container">
-    <?php foreach ($rows as $row) { ?>
-        <div class="row">
-            <?php foreach ($row as $author) { ?>
-                <?php $author_id = $author['author'];
-                $author_title = get_the_title($author_id);
-                $author_image = get_field('author_profile', $author_id);
-                $author_expertise = get_field('expertise', $author_id);
-                $author_link = get_permalink($author_id); ?>
-                <div class="col single-author-col">
-                    <div class="single-author-container">
-                        <a href="<?php echo $author_link; ?>" target="_blank">
-                            <img class="author-image" src="<?php echo $author_image ?>" alt="<?php echo $author_title ?>" />
-                        </a>
-                        <p><?php echo esc_html($author_title); ?></p>
-                        <p><?php echo esc_html($author_expertise) ?></p>
+<section class="author-block-container">
+    <div class="container">
+        <?php foreach ($rows as $row) { ?>
+            <div class="row justify-content-center">
+                <?php foreach ($row as $author) { ?>
+                    <?php
+                        $author_id = $author['author'];
+                        $author_title = get_the_title($author_id);
+                        $author_image = get_field('author_profile', $author_id);
+                        $author_expertise = get_field('expertise', $author_id);
+                        $author_link = get_permalink($author_id);
+                    ?>
+                    <div class="col single-author-col">
+                        <div class="single-author-container">
+                            <a href="<?php echo $author_link; ?>" target="_blank">
+                                <img class="author-image" src="<?php echo $author_image ?>" alt="<?php echo $author_title ?>" />
+                            </a>
+                            <p><?php echo esc_html($author_title); ?></p>
+                            <p><?php echo esc_html($author_expertise) ?></p>
+                        </div>
                     </div>
-                </div>
-            <?php } ?>
-        </div>
-    <?php } ?>
-</div>
+                <?php } ?>
+            </div>
+        <?php } ?>
+    </div>
+</section>
