7f6b8e1f29375786610db741b39b0953a35d89d7
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-12-08 20:18:13 +0200

    kakis footer e responsive

--- footer.php
+++ footer.php
@@ -14,9 +14,9 @@
 <footer class="footer-container pt-5">
     <div class="container pt-3">
         <div class="row justify-content-center">
-            <div class="col-8">
+            <div class="col-lg-8 col-10">
                 <div class="row">
-                    <div class="col-8 px-1">
+                    <div class="col-lg-8 col-10 px-1">
                         <button data-bs-toggle="modal" data-bs-target="#contact-us">
                             about us
                         </button>
@@ -26,7 +26,18 @@
                         <button data-bs-toggle="modal" data-bs-target="#contact-us">
                             Join our collective
                         </button>
-                        <div class="row">
+                        <div class="d-block d-lg-none">
+                            <button data-bs-toggle="modal" data-bs-target="#contact-us">
+                                    Mission
+                            </button>
+                            <button data-bs-toggle="modal" data-bs-target="#contact-us">
+                                Vision
+                            </button>
+                            <button data-bs-toggle="modal" data-bs-target="#contact-us">
+                                Causes
+                            </button>
+                        </div>
+                        <div class="row d-lg-flex d-none">
                             <div class="col-6">
                                 <button data-bs-toggle="modal" data-bs-target="#contact-us">
                                     contact us
@@ -75,8 +86,29 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="row d-lg-none d-flex">
+                            <div class="col-6">
+                                <button data-bs-toggle="modal" data-bs-target="#contact-us">
+                                    contact us
+                                </button>
+                            </div>
+                            <div class="col-6">
+                                <button class="full" data-bs-toggle="modal" data-bs-target="#contact-us">
+                                    Be our advocate
+                                </button>
+                            </div>
+                        </div>
+                        <div class="row d-lg-none d-flex">
+                            <button class="full text-center custom-width donate" data-bs-toggle="modal"
+                                data-bs-target="#contact-us">
+                                <img class="heart"
+                                    src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/heart-green.svg"
+                                    alt="heart-donate">
+                                Donate
+                            </button>
+                        </div>
                     </div>
-                    <div class="col-4 px-1">
+                    <div class="col-4 px-1 d-lg-flex d-none">
                         <div class="custom-rotate">
                             <button data-bs-toggle="modal" data-bs-target="#contact-us">
                                 Mission
