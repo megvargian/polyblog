4af31422dcf8f7d6ae37f381094569ca2448834f
hrag <hrag.h@hotmail.com>
2024-12-09 17:47:34 +0100

    block 1 repeater

--- blocks/single_featured_article_block.php
+++ blocks/single_featured_article_block.php
@@ -3,45 +3,54 @@
  * HomePage Single Featured Article Block Template
  */
 
-$featured_article_block = get_fields();
+$featured_articles = get_field('featured_articles');
 
-$title = get_the_title($featured_article_block['selected_article']);
-$article_link = get_permalink($featured_article_block['selected_article']);
-$tags = get_the_tags($featured_article_block['selected_article']);
-$categories = get_the_category($featured_article_block['selected_article']);
-$author_name = get_the_title(get_field('author', $featured_article_block['selected_article']));
-$thumbnail = get_field('article_thumbnail', $featured_article_block['selected_article']);
-?>
-<div class="row single-featured-article-container">
-    <div class="col-5">
-        <a href="<?php echo $article_link; ?>">
-            <img src="<?php echo $thumbnail; ?>" alt="<?php echo $title; ?>" />
-        </a>
-    </div>
-    <div class="col-7 right-container">
-        <div class="categories">
-            <?php foreach ($categories as $category) { ?>
-                <span class="category">
-                    <?php echo esc_html($category->name); ?>
-                </span>
-            <?php } ?>
-        </div>
-        <div class="title align-text-arabic">
-            <a href="<?php echo $article_link; ?>">
-                <?php echo $title; ?>
-            </a>
-        </div>
-        <div class="author-tags">
-            <div class="author">
-                <p>
-                    <?php echo $author_name ?>
-                </p>
+if ($featured_articles): ?>
+    <div class="row featured-articles-container">
+        <?php foreach ($featured_articles as $featured_article):
+            $article = $featured_article['article'];
+            $title = get_the_title($article);
+            $article_link = get_permalink($article);
+            $tags = get_the_tags($article);
+            $categories = get_the_category($article);
+            $author_name = get_the_title(get_field('author', $article));
+            $thumbnail = get_field('article_thumbnail', $article);
+            ?>
+            <div class="row single-featured-article-container">
+                <div class="col-5">
+                    <a href="<?php echo esc_url($article_link); ?>">
+                        <img src="<?php echo esc_url($thumbnail); ?>" alt="<?php echo esc_attr($title); ?>" />
+                    </a>
+                </div>
+                <div class="col-7 right-container">
+                    <div class="categories">
+                        <?php if ($categories) {
+                            foreach ($categories as $category) { ?>
+                                <span class="category">
+                                    <?php echo esc_html($category->name); ?>
+                                </span>
+                            <?php }
+                        } ?>
+                    </div>
+                    <div class="title align-text-arabic">
+                        <a href="<?php echo esc_url($article_link); ?>">
+                            <?php echo esc_html($title); ?>
+                        </a>
+                    </div>
+                    <div class="author-tags">
+                        <div class="author">
+                            <p><?php echo esc_html($author_name); ?></p>
+                        </div>
+                        <div class="tags">
+                            <?php if ($tags) {
+                                foreach ($tags as $tag) {
+                                    echo esc_html($tag->name) . ' ';
+                                }
+                            } ?>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="tags">
-                <?php foreach ($tags as $tag) { ?>
-                    <?php echo esc_html($tag->name); ?>
-                <?php } ?>
-            </div>
-        </div>
+        <?php endforeach; ?>
     </div>
-</div>
+<?php endif; ?>
