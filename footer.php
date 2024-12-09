ea21e6fdb1eab76823d9f13ac2d648d2d4dbeba5
hrag <hrag.h@hotmail.com>
2024-12-09 17:06:16 +0100

    ammazzami

--- footer.php
+++ footer.php
@@ -159,24 +159,26 @@
 <?php wp_footer(); ?>
 </body>
 <script>
-    const button = document.getElementById('menu-button');
-    const initialPosition = button.offsetTop;
-    let lastScrollTop = 0;
-
-    window.addEventListener('scroll', () => {
-      const currentScroll = window.scrollY;
-
-      if (currentScroll > initialPosition) {
-        button.classList.add('fixed-menu-button');
-      } else {
-        button.classList.remove('fixed-menu-button');
-      }
-
-      if (currentScroll < lastScrollTop) {
-        button.classList.remove('fixed-button');
-      }
-
-      lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+    jQuery(document).ready(function ($) {
+        const button = document.getElementById('menu-button');
+        const initialPosition = button.offsetTop;
+        let lastScrollTop = 0;
+    
+        window.addEventListener('scroll', () => {
+          const currentScroll = window.scrollY;
+    
+          if (currentScroll > initialPosition) {
+            button.classList.add('fixed-menu-button');
+          } else {
+            button.classList.remove('fixed-menu-button');
+          }
+    
+          if (currentScroll < lastScrollTop) {
+            button.classList.remove('fixed-button');
+          }
+    
+          lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+        });
     });
 </script>
 </html>
