60db3c262b6947c4ef45b0ae8591d9849d8ca59e
hrag <hrag.h@hotmail.com>
2024-12-09 14:07:05 +0100

    porco dio

--- footer.php
+++ footer.php
@@ -161,13 +161,22 @@
 <script>
     const button = document.getElementById('menu-button');
     const initialPosition = button.offsetTop;
+    let lastScrollTop = 0;
 
     window.addEventListener('scroll', () => {
-        if (window.scrollY > initialPosition) {
-            button.classList.add('fixed-menu-button');
-        } else {
-            button.classList.remove('fixed-menu-button');
-        }
+      const currentScroll = window.scrollY;
+
+      if (currentScroll > initialPosition) {
+        button.classList.add('fixed-menu-button');
+      } else {
+        button.classList.remove('fixed-menu-button');
+      }
+
+      if (currentScroll < lastScrollTop) {
+        button.classList.remove('fixed-button');
+      }
+
+      lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
     });
 </script>
 </html>
