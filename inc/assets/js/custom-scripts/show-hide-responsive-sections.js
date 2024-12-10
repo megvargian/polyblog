26a2d5b4aa1a2677acf2aab13bfb4d7ac521f417
hrag <hrag.h@hotmail.com>
2024-12-10 09:33:14 +0100

    hide single article menu on mobile

--- inc/assets/js/custom-scripts/show-hide-responsive-sections.js
+++ inc/assets/js/custom-scripts/show-hide-responsive-sections.js
@@ -2,21 +2,17 @@ jQuery(document).ready(function ($) {
   $(window)
     .on('resize', function () {
       if ($(window).width() < 575) { // MOBILE
+        // switch home featured articles slider and normal view
         const homeFeaturedArticlesDesktop = document.getElementById('home-featured-articles-desktop');
         const homeFeaturedArticlesMobile = document.getElementById('home-featured-articles-mobile');
         homeFeaturedArticlesDesktop.style.display = 'none';
         homeFeaturedArticlesMobile.style.display = 'block';
-
-        const threeFeaturedArticlesBlock = document.getElementById('three-featured-articles-block');
-        threeFeaturedArticlesBlock.style.display = 'none';
       } else { // DESKTOP
+        // switch home featured articles slider and normal view
         const homeFeaturedArticlesDesktop = document.getElementById('home-featured-articles-desktop');
         const homeFeaturedArticlesMobile = document.getElementById('home-featured-articles-mobile');
         homeFeaturedArticlesDesktop.style.display = 'block';
         homeFeaturedArticlesMobile.style.display = 'none';
-
-        const threeFeaturedArticlesBlock = document.getElementById('three-featured-articles-block');
-        threeFeaturedArticlesBlock = 'block';
       }
     })
     .trigger('resize');
