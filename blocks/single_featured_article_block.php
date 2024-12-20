0f23ce32cd173400a74de8781c62c53a7425051c
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-12-20 14:17:24 +0200

    test

--- blocks/single_featured_article_block.php
+++ blocks/single_featured_article_block.php
@@ -17,37 +17,39 @@ if ($featured_articles): ?>
                 $author_name = get_the_title(get_field('author', $article));
                 $thumbnail = get_field('article_thumbnail', $article);
                 ?>
-                <div class="row single-featured-article-container ar-bold">
-                    <div class="col-5">
-                        <a href="<?php echo esc_url($article_link); ?>">
-                            <img src="<?php echo esc_url($thumbnail); ?>" alt="<?php echo esc_attr($title); ?>" />
-                        </a>
-                    </div>
-                    <div class="col-7 p-5 right-container">
-                        <div class="categories">
-                            <?php if ($categories) {
-                                foreach ($categories as $category) { ?>
-                                    <span class="category ar-regular">
-                                        <?php echo esc_html($category->name); ?>
-                                    </span>
-                                <?php }
-                            } ?>
-                        </div>
-                        <div class="title align-text-arabic">
+                <div class="col-12">
+                    <div class="row single-featured-article-container ar-bold">
+                        <div class="col-5">
                             <a href="<?php echo esc_url($article_link); ?>">
-                                <?php echo esc_html($title); ?>
+                                <img src="<?php echo esc_url($thumbnail); ?>" alt="<?php echo esc_attr($title); ?>" />
                             </a>
                         </div>
-                        <div class="author-tags">
-                            <div class="author">
-                                <p><?php echo esc_html($author_name); ?></p>
-                            </div>
-                            <div class="tags">
-                                <?php if ($tags) {
-                                    foreach ($tags as $tag) {
-                                        echo esc_html($tag->name) . ' ';
-                                    }
+                        <div class="col-7 p-5 right-container">
+                            <div class="categories">
+                                <?php if ($categories) {
+                                    foreach ($categories as $category) { ?>
+                                        <span class="category ar-regular">
+                                            <?php echo esc_html($category->name); ?>
+                                        </span>
+                                    <?php }
                                 } ?>
+                            </div>
+                            <div class="title align-text-arabic">
+                                <a href="<?php echo esc_url($article_link); ?>">
+                                    <?php echo esc_html($title); ?>
+                                </a>
+                            </div>
+                            <div class="author-tags">
+                                <div class="author">
+                                    <p><?php echo esc_html($author_name); ?></p>
+                                </div>
+                                <div class="tags">
+                                    <?php if ($tags) {
+                                        foreach ($tags as $tag) {
+                                            echo esc_html($tag->name) . ' ';
+                                        }
+                                    } ?>
+                                </div>
                             </div>
                         </div>
                     </div>
