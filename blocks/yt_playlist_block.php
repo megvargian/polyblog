21ef03513a40a838cb13293f0f2ed59d005064e6
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-12-20 18:01:34 +0200

    fix youtube section

--- blocks/yt_playlist_block.php
+++ blocks/yt_playlist_block.php
@@ -8,21 +8,21 @@ $yt_playlist_block = get_fields();
 <section class="w-100 youtube-playlist-bg d-none d-lg-block">
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col youtube-playlist-button-col justify-content-center d-flex">
+            <div class="col youtube-playlist-button-col justify-content-center d-flex align-items-center">
                 <a href="<?php echo $yt_playlist_block['title_link'] ?>" target="_blank">
                     <img class="youtube-playlist-btn" src="<?php echo $yt_playlist_block['title']; ?>" alt="YouTube">
                 </a>
             </div>
-            <div class="col youtube-playlist-button-col justify-content-center d-flex">
+            <div class="col youtube-playlist-button-col justify-content-center d-flex align-items-center">
                 <a href="<?php echo $yt_playlist_block['yt_link'] ?>" target="_blank">
                     <img class="youtube-playlist-btn"
-                        src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/youtube-production-icon.svg" alt="YouTube">
+                        src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/youtube-production-icon.svg" alt="YouTube">
                 </a>
             </div>
-            <div class="col youtube-playlist-button-col justify-content-center d-flex">
+            <div class="col youtube-playlist-button-col justify-content-center d-flex align-items-center">
                 <img class="youtube_featured_image" src="<?php echo $yt_playlist_block['yt_image']; ?>" alt="YouTube">
             </div>
-            <div class="col youtube-playlist-button-col justify-content-center d-flex">
+            <div class="col youtube-playlist-button-col justify-content-center d-flex align-items-center">
                 <a href="<?php echo $yt_playlist_block['episodes_link'] ?>" target="_blank">
                     <img class="youtube-playlist-btn" src="<?php echo $yt_playlist_block['episodes']; ?>" alt="YouTube">
                 </a>
