61e69b4561a302f25cf09c67a5109cfd3d829fea
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-11 09:02:10 +0200

    change the youtube section

--- blocks/yt_playlist_block.php
+++ blocks/yt_playlist_block.php
@@ -6,6 +6,33 @@
 $yt_playlist_block = get_fields();
 ?>
 <section class="w-100 youtube-playlist-bg d-none d-lg-block">
+    <div class="container py-4">
+        <div class="row justify-content-between align-items-center">
+            <div class="col">
+                <h3 class="en-bold">our special <br> productions</h3>
+                <p></p>
+            </div>
+            <div class="col">
+                <h3 class="ar-bold">انتاجاتنـــــــــــــــــــــ <br> ـــــا الخاصـــــــــــــة</h3>
+            </div>
+        </div>
+        <div class="row" style="min-height: 40rem">
+            <div class="col d-flex justify-content-center align-items-center">
+                <img class="youtube-playlist-btn" src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/youtube-production-icon.svg" alt="YouTube">
+            </div>
+        </div>
+        <div class="row justify-content-between align-items-center">
+            <div class="col">
+                <p class="en-regular">polyblog reduces special contenpolyblog reduces special content...t...</p>
+                <p></p>
+            </div>
+            <div class="col">
+                <p class="ar-regular">انتاجاتنـــــــــــــــــــــ
+ـــــا الخاصـــــــــــــةانتاجاتنـــــــــــــــــــــ
+ـــــا الخاصـــــــــــــة</p>
+            </div>
+        </div>
+    </div>
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col youtube-playlist-button-col justify-content-center d-flex align-items-center">
