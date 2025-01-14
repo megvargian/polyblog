6b9b80e6784e1ebc01076bed529f33babd9bea8d
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-14 20:26:42 +0200

    test

--- blocks/three_featured_articles_block.php
+++ blocks/three_featured_articles_block.php
@@ -18,6 +18,7 @@ $query = new WP_Query($args);
     <div class="container">
         <div class="row position-relative z-1 p-lg-5 p-2">
             <?php
+            $count=0;
             if ($query->have_posts()) {
                 while ($query->have_posts()) {
                     $query->the_post();
@@ -25,8 +26,9 @@ $query = new WP_Query($args);
                     $article_link = get_permalink();
                     $categories = get_the_category();
                     $article_thumbnail = get_field('article_thumbnail', get_the_ID());
+                    $count++;
                     ?>
-                    <div class="col-4 p-3 article-container mx-lg-0 mx-5">
+                    <div class="col-4 p-3 article-container <?php echo $count === 1 ? 'hovered' : '' ?>">
                         <a class="position-relative" href="<?php echo $article_link; ?>" target="_blank">
                             <img src="<?php echo $article_thumbnail; ?>" alt="<?php echo $article_title; ?>">
                             <div class="hover-text">
@@ -41,3 +43,16 @@ $query = new WP_Query($args);
         </div>
     </div>
 </section>
+
+<script>
+    $(document).ready(function () {
+        $('.article-container').hover(
+            function () {
+            $(this).addClass('hovered'); // Add class on hover
+            },
+            function () {
+            $(this).removeClass('hovered'); // Remove class when mouse leaves
+            }
+        );
+    });
+</script>
