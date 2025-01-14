5dfbe37f7239f81983c2a282fd1cd8eb13587966
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-14 22:52:29 +0200

    test

--- blocks/author_swiper_block.php
+++ blocks/author_swiper_block.php
@@ -5,43 +5,45 @@
 
 $author_swiper_block = get_fields();
 $authors_list = $author_swiper_block['authors_list'];
+if(isMob()){
 ?>
-<div class="container-fluid author-swiper-container py-5">
-    <div class="row justify-content-center position-relative">
-        <div class="col-11">
-            <div class="swiper author_swiper_block">
-                <div class="swiper-wrapper">
-                    <?php foreach ($authors_list as $author) { ?>
-                        <div class="swiper-slide">
-                            <?php $author_id = $author['author'];
-                            $author_title = get_the_title($author_id);
-                            $author_image = get_field('author_profile', $author_id);
-                            $author_link = get_permalink($author_id); ?>
-                            <!-- <p><?php echo esc_html($author_title); ?></p> -->
-                            <a href="<?php echo $author_link; ?>" target="_blank">
-                                <div class="author-image">
-                                    <img class="img-fluid" src="<?php echo $author_image ?>"
-                                    alt="<?php echo $author_title ?>" />
-                                </div>
-                            </a>
-                        </div>
-                    <?php } ?>
+    <div class="container-fluid author-swiper-container py-5 d-lg-block d-none">
+        <div class="row justify-content-center position-relative">
+            <div class="col-11">
+                <div class="swiper author_swiper_block">
+                    <div class="swiper-wrapper">
+                        <?php foreach ($authors_list as $author) { ?>
+                            <div class="swiper-slide">
+                                <?php $author_id = $author['author'];
+                                $author_title = get_the_title($author_id);
+                                $author_image = get_field('author_profile', $author_id);
+                                $author_link = get_permalink($author_id); ?>
+                                <!-- <p><?php echo esc_html($author_title); ?></p> -->
+                                <a href="<?php echo $author_link; ?>" target="_blank">
+                                    <div class="author-image">
+                                        <img class="img-fluid" src="<?php echo $author_image ?>"
+                                        alt="<?php echo $author_title ?>" />
+                                    </div>
+                                </a>
+                            </div>
+                        <?php } ?>
+                    </div>
                 </div>
             </div>
+            <div class="swiper-button-prev swiper-button-prev-last-part-author"></div>
+            <div class="swiper-button-next swiper-button-next-last-part-author"></div>
         </div>
-        <div class="swiper-button-prev swiper-button-prev-last-part-author"></div>
-        <div class="swiper-button-next swiper-button-next-last-part-author"></div>
     </div>
-</div>
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        const swiper = new Swiper('.author_swiper_block', {
-            loop: true,
-            slidesPerView: 4,
-            navigation: {
-                nextEl: '.swiper-button-next-last-part-author',
-                prevEl: '.swiper-button-prev-last-part-author',
-            },
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            const swiper = new Swiper('.author_swiper_block', {
+                loop: true,
+                slidesPerView: 4,
+                navigation: {
+                    nextEl: '.swiper-button-next-last-part-author',
+                    prevEl: '.swiper-button-prev-last-part-author',
+                },
+            });
         });
-    });
-</script>
+    </script>
+<?php } ?>
--- blocks/message_from_editor_block.php
+++ blocks/message_from_editor_block.php
@@ -6,10 +6,30 @@
 $message_from_deitor_fields = get_fields();
 ?>
 <section class="message-for-editor-section">
-    <img class="w-100 d-block" src="<?php echo $message_from_deitor_fields['main_image']; ?>" alt="message_from_directo_bannerr">
+    <img class="w-100 d-lg-block d-none" src="<?php echo $message_from_deitor_fields['main_image']; ?>" alt="message_from_directo_bannerr">
+    <div class="d-lg-none d-block">
+        <div class="container">
+            <div class="row justify-content-between d-flex">
+                <div class="col text-right">
+                    <h4>A Message from <br> the editor</h4>
+                </div>
+                <div class="col">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <div class="mx-1 languages en-regular">
+                        ع
+                        </div>
+                        <div class="mx-1 languages en-regular">
+                        En
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container-fluid py-4 px-4">
         <div class="row">
-            <div class="col justify-content-center align-items-center d-flex">
+            <div class="col justify-content-center align-items-center d-lg-flex d-none">
                <p class="en">
                     <?php echo $message_from_deitor_fields['en_text']; ?>
                </p>
