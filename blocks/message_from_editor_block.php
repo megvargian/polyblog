5f2a4370efd43773b5ca8b5abc324348da6d8207
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-14 22:57:43 +0200

    test

--- blocks/message_from_editor_block.php
+++ blocks/message_from_editor_block.php
@@ -7,27 +7,22 @@ $message_from_deitor_fields = get_fields();
 ?>
 <section class="message-for-editor-section">
     <img class="w-100 d-lg-block d-none" src="<?php echo $message_from_deitor_fields['main_image']; ?>" alt="message_from_directo_bannerr">
-    <div class="d-lg-none d-block">
-        <div class="container">
-            <div class="row justify-content-between d-flex">
-                <div class="col text-right">
-                    <h4 class="en-bold">A Message from <br> the editor</h4>
-                </div>
-                <div class="col">
-                    <div class="d-flex justify-content-center align-items-center">
-                        <div class="mx-1 languages en-regular">
-                        ع
-                        </div>
-                        <div class="mx-1 languages en-regular">
-                        En
-                        </div>
-
+    <div class="container-fluid py-4 px-4">
+        <div class="row justify-content-between d-flex d-lg-none">
+            <div class="col">
+                <div class="d-flex justify-content-center align-items-center">
+                    <div class="mx-1 languages en-regular">
+                    ع
+                    </div>
+                    <div class="mx-1 languages en-regular">
+                    En
                     </div>
                 </div>
             </div>
+            <div class="col text-right">
+                <h4 class="en-bold">A Message from <br> the editor</h4>
+            </div>
         </div>
-    </div>
-    <div class="container-fluid py-4 px-4">
         <div class="row">
             <div class="col justify-content-center align-items-center d-lg-flex d-none">
                <p class="en">
