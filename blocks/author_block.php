165079e928c613382df2b0dfa1776ee290f50fce
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-15 00:40:20 +0200

    test

--- blocks/author_block.php
+++ blocks/author_block.php
@@ -41,34 +41,36 @@ $rows = array_chunk($authors_list, 4);
                 <div class="d-flex justify-content-center align-items-center px-sm-5 px-2 h-100">
                     <div class="inner-author mx-auto">
                         <p class="text-center" style="color: #fff;">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam doloremque exercitationem qui laboriosam quasi ducimus fuga atque tenetur maiores quaerat, suscipit, vel similique porro, officiis harum pariatur sapiente. Nihil, deleniti?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam doloremque exercitationem qui
                         </p>
                     </div>
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-6 px-0">
                 <img class="w-100 h-100 d-block single-red-border-left" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/board-img-mobile.png" alt="article-title">
             </div>
         </div>
         <div class="row my-3 justify-content-center">
-            <?php foreach ($row as $author) { ?>
-                <?php
-                $author_id = $author['author'];
-                $author_title = get_the_title($author_id);
-                $author_image = get_field('author_profile', $author_id);
-                $author_expertise = get_field('expertise', $author_id);
-                $author_link = get_permalink($author_id);
-                ?>
-                <div class="col-4 single-author-col">
-                    <a href="<?php echo $author_link; ?>" target="_blank">
-                        <div class="single-author-container">
-                            <div class="author-image">
-                                <img class="img-fluid" src="<?php echo $author_image ?>"
-                                    alt="<?php echo $author_title ?>" />
+            <?php foreach ($rows as $row) { ?>
+                <?php foreach ($row as $author) { ?>
+                    <?php
+                    $author_id = $author['author'];
+                    $author_title = get_the_title($author_id);
+                    $author_image = get_field('author_profile', $author_id);
+                    $author_expertise = get_field('expertise', $author_id);
+                    $author_link = get_permalink($author_id);
+                    ?>
+                    <div class="col-4 single-author-col">
+                        <a href="<?php echo $author_link; ?>" target="_blank">
+                            <div class="single-author-container">
+                                <div class="author-image">
+                                    <img class="img-fluid" src="<?php echo $author_image ?>"
+                                        alt="<?php echo $author_title ?>" />
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>
+                <?php } ?>
             <?php } ?>
         </div>
     </div>
