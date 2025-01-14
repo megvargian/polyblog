175823a65c4a4f7b62d4387ba5f17400e7338be0
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-14 22:19:27 +0200

    change the bg-img block

--- blocks/bg_img_block.php
+++ blocks/bg_img_block.php
@@ -5,12 +5,21 @@
 
 $bg_img_block = get_fields();
 ?>
-<section class="">
+<section class="d-lg-block d-none">
     <?php if($bg_img_block['main_image_url']) {?>
         <a href="<?php echo $bg_img_block['main_image_url'] ?>">
             <img class="w-100 d-block" src="<?php echo $bg_img_block['main_image']; ?>" alt="">
         </a>
     <?php } else { ?>
         <img class="w-100 d-block" src="<?php echo $bg_img_block['main_image']; ?>" alt="">
+    <?php } ?>
+</section>
+<section class="d-lg-none d-block">
+    <?php if($bg_img_block['main_image_url_mobile']) {?>
+        <a href="<?php echo $bg_img_block['main_image_url'] ?>">
+            <img class="w-100 d-block" src="<?php echo $bg_img_block['main_image_mobile']; ?>" alt="">
+        </a>
+    <?php } else { ?>
+        <img class="w-100 d-block" src="<?php echo $bg_img_block['main_image_mobile']; ?>" alt="">
     <?php } ?>
 </section>
