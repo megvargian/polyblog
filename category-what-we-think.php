b92b97d7714d25f80d8e3994b49d35022bcbcfa6
hrag <hrag.h@hotmail.com>
2025-01-21 08:58:32 +0100

    what we think

--- category-what-we-think.php
+++ category-what-we-think.php
@@ -5,19 +5,41 @@
             <img class="header" src="https://polybloglb.com/wp-content/uploads/2025/01/what-we-think.png" alt="what we think" />
         </div>
     </div>
-    <div class="category-posts">
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <div class="row">
-                    <div class="col">
-                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                        <p><?php the_excerpt(); ?></p>
+    <div class="row my-2">
+        <div class="col">
+            <input placeholder="Search" />
+        </div>
+    </div>
+    <div class="what-we-think-posts">
+        <div class="row">
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : the_post(); ?>
+                    <div class="col-md-6 mb-4">
+                        <div class="card">
+                            <?php if (has_post_thumbnail()) : ?>
+                                <div class="card-img-top">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_post_thumbnail('medium', ['class' => 'img-fluid']); ?>
+                                    </a>
+                                </div>
+                            <?php endif; ?>
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                </h5>
+                                <p class="card-text"><?php the_excerpt(); ?></p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            <?php endwhile; ?>
-        <?php else : ?>
-            <p>No posts found in this category.</p>
+            <?php if ($wp_query->current_post % 2 == 1) : ?>
+        </div>
+        <div class="row">
         <?php endif; ?>
+    <?php endwhile; ?>
+<?php else : ?>
+    <p>No posts found in this category.</p>
+<?php endif; ?>
+        </div>
     </div>
 </div>
 <?php get_footer(); ?>
