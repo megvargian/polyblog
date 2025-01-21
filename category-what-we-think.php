937da893dd65868b607d6bf4ae3a727f11e7a9ca
hrag <hrag.h@hotmail.com>
2025-01-21 08:39:19 +0100

    what we think

---
+++ category-what-we-think.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="https://polybloglb.com/wp-content/uploads/2025/01/what-we-think.png" alt="what we think" />
+        </div>
+    </div>
+    <div class="category-posts">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div class="row">
+                    <div class="col">
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <p><?php the_excerpt(); ?></p>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        <?php else : ?>
+            <p>No posts found in this category.</p>
+        <?php endif; ?>
+    </div>
+</div>
+<?php get_footer(); ?>
