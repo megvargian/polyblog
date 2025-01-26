8af06f8666f29eca091a7fc15fa557f986322d20
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-26 13:36:26 +0200

    add new search-authors desktop

--- search-authors-page.php
+++ search-authors-page.php
@@ -7,7 +7,7 @@ get_header();
 ?>
 <section class="search-authors-page">
     <div class="container">
-        <div class="row mt-2 mb-5">
+        <div class="row pt-2 mb-5">
             <div class="col-12">
                 <div class="white-line"></div>
             </div>
@@ -21,6 +21,20 @@ get_header();
                     </h1>
                 </div>
             </div>
+        </div>
+        <div class="row justify-content-center">
+            <?php for($i=0; i<15; i++){ ?>
+                <div class="col-4 text-center">
+                    <div class="single-author-block d-flex justify-content-center align-items-center p-4">
+                        <div>
+                            <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/iamges/search-authors-img.png" alt="">
+                            <h4 class="ar-bold">د. رمزي أبو اسماعيل</h4>
+                            <p class="ar-regular">السياسة / الشرق الأوسط / الحرب</p>
+                            <a class="mt-3 view-more-btn" href="#">View Profile</a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
         </div>
     </div>
 </section>
