bd1213c01a909fec3678f5fb862c992e49a80a5e
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-26 13:08:27 +0200

    add new search-authors desktop

---
+++ search-authors-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Search Authors Page
+ */
+
+get_header();
+?>
+<section class="search-authors-page">
+    <div class="container">
+        <div class="row text-center py-5">
+            <div class="col-12">
+                <div class="bg-black">
+                    <h1>
+                        <span class="en-bold">MEET OUR VOICES</span>
+                        <span class="ar-bold">تعـــــــــرف على أصــــــــــــــــــواتنا</span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+get_footer();
