0c251ac53e6e6e7a72a650d8e25ae2b0e3126f05
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-26 17:50:50 +0200

    add our segements page

--- our-segements-page.php
+++ our-segements-page.php
@@ -22,24 +22,99 @@ get_header();
                 </div>
             </div>
         </div>
-        <div class="row pb-5">
-            <div class="col-12 pb-5">
-                <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/our-segements-first-img.png" alt="">
-            </div>
-            <div class="col-6 text-left">
-                <h5 class="en-bold">SYNOPSIS</h5>
-                <p class="en-regular">
-                    In Syria, following the recent ousting of President Bashar al-Assad,In Syria, following the recent ousting of President Bashar al-Assad,In Syria
-                </p>
-            </div>
-            <div class="col-6 text-right">
-                <h5 class="ar-bold">ملخص</h5>
-                <p class="ar-regular">
-                لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار
-                </p>
+    </div>
+    <section>
+        <div class="container">
+            <div class="row pb-5">
+                <div class="col-12 pb-5">
+                    <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/our-segements-first-img.png" alt="">
+                </div>
+                <div class="col-6 text-left">
+                    <h5 class="en-bold">SYNOPSIS</h5>
+                    <p class="en-regular">
+                        In Syria, following the recent ousting of President Bashar al-Assad,In Syria, following the recent ousting of President Bashar al-Assad,In Syria
+                    </p>
+                </div>
+                <div class="col-6 text-right">
+                    <h5 class="ar-bold">ملخص</h5>
+                    <p class="ar-regular">
+                    لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
+    <section class="bg-orange-section">
+        <div class="white-line"></div>
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-12">
+                    <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/our-segements-first-img.png" alt="">
+                </div>
+            </div>
+            <div class="row custom-border-radius px-4">
+                <div class="col-6 text-left">
+                    <h5 class="en-bold">SYNOPSIS</h5>
+                    <p class="en-regular">
+                        In Syria, following the recent ousting of President Bashar al-Assad,In Syria, following the recent ousting of President Bashar al-Assad,In Syria
+                    </p>
+                </div>
+                <div class="col-6 text-right">
+                    <h5 class="ar-bold">ملخص</h5>
+                    <p class="ar-regular">
+                    لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="white-line"></div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row pb-5">
+                <div class="col-12 pb-5">
+                    <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/our-segements-first-img.png" alt="">
+                </div>
+                <div class="col-6 text-left">
+                    <h5 class="en-bold">SYNOPSIS</h5>
+                    <p class="en-regular">
+                        In Syria, following the recent ousting of President Bashar al-Assad,In Syria, following the recent ousting of President Bashar al-Assad,In Syria
+                    </p>
+                </div>
+                <div class="col-6 text-right">
+                    <h5 class="ar-bold">ملخص</h5>
+                    <p class="ar-regular">
+                    لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-orange-section">
+        <div class="white-line"></div>
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-12">
+                    <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/our-segements-first-img.png" alt="">
+                </div>
+            </div>
+            <div class="row custom-border-radius px-4">
+                <div class="col-6 text-left">
+                    <h5 class="en-bold">SYNOPSIS</h5>
+                    <p class="en-regular">
+                        In Syria, following the recent ousting of President Bashar al-Assad,In Syria, following the recent ousting of President Bashar al-Assad,In Syria
+                    </p>
+                </div>
+                <div class="col-6 text-right">
+                    <h5 class="ar-bold">ملخص</h5>
+                    <p class="ar-regular">
+                    لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="white-line"></div>
+    </section>
 </section>
 
 <?php
