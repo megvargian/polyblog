731fb7f934d32602e67ec0beebb5e07b6523f481
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-26 17:41:31 +0200

    add our segements page

---
+++ our-segements-page.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Our Segements Page
+ */
+
+get_header();
+?>
+<section class="our-segements-page">
+    <div class="container">
+        <div class="row pt-2 mb-5">
+            <div class="col-12">
+                <div class="white-line"></div>
+            </div>
+        </div>
+        <div class="row text-center py-5">
+            <div class="col-12">
+                <div class="bg-black">
+                    <h1>
+                        <span class="en-bold">OUR SEGEMENTS</span>
+                        <span class="ar-bold">كيــــف منشـــوف</span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+        <div class="row pb-5">
+            <div class="col-12 pb-5">
+                <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/our-segements-first-img.png" alt="">
+            </div>
+            <div class="col-6 text-right">
+                <h5 class="ar-bold">ملخص</h5>
+                <p class="ar-regular">
+                لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار
+                </p>
+            </div>
+            <div class="col-6 text-left">
+                <h5 class="en-bold">SYNOPSIS</h5>
+                <p class="en-regular">
+                    In Syria, following the recent ousting of President Bashar al-Assad,In Syria, following the recent ousting of President Bashar al-Assad,In Syria
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+get_footer();
