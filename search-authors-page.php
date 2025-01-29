adbb47b4c639905ac78c0f61faaa193b960f7e89
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-29 16:42:13 +0200

    add hover effect on search for authors

--- search-authors-page.php
+++ search-authors-page.php
@@ -24,15 +24,17 @@ get_header();
         </div>
         <div class="row justify-content-center pt-5">
             <?php for($i=0; $i<15; $i++){ ?>
-                <div class="col-4 text-center mb-4">
-                    <div class="single-author-block d-flex justify-content-center align-items-center p-4">
-                        <div>
-                            <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/demo-profile.png" alt="">
-                            <h4 class="ar-bold pt-2">د. رمزي أبو اسماعيل</h4>
-                            <p class="ar-regular">السياسة / الشرق الأوسط / الحرب</p>
-                            <a class="mt-3 view-more-btn en-regular" href="#">View Profile</a>
+                <div class="col-4 text-center mb-4 hovered-single-author">
+                    <a href="#">
+                        <div class="single-author-block d-flex justify-content-center align-items-center p-4">
+                            <div>
+                                <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/demo-profile.png" alt="">
+                                <h4 class="ar-bold pt-2">د. رمزي أبو اسماعيل</h4>
+                                <p class="ar-regular">السياسة / الشرق الأوسط / الحرب</p>
+                                <a class="mt-3 view-more-btn en-regular" href="#">View Profile</a>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             <?php } ?>
         </div>
