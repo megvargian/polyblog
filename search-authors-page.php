debce42bcb7f47e1f848a54c8f2602b8bed6c980
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-01-29 17:38:17 +0200

    test

--- search-authors-page.php
+++ search-authors-page.php
@@ -54,6 +54,42 @@ get_header();
                 </div>
             </div>
         </div>
+        <div class="row justify-content-center pt-5 d-lg-none d-flex">
+            <div class="swiper search-authors-swiper">
+                <div class="swiper-wrapper">
+                    <?php for($i=0; $i<15; $i++){ ?>
+                        <div class="swiper-slide text-center mb-4 hovered-single-author">
+                            <div class="single-author-block d-flex justify-content-center align-items-center p-4">
+                                <div>
+                                    <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/demo-profile.png" alt="">
+                                    <h4 class="ar-bold pt-2">د. رمزي أبو اسماعيل</h4>
+                                    <p class="ar-regular">السياسة / الشرق الأوسط / الحرب</p>
+                                    <a class="mt-3 view-more-btn en-regular" href="#">View Profile</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center pt-5 d-lg-none d-flex">
+            <div class="swiper search-authors-swiper">
+                <div class="swiper-wrapper">
+                    <?php for($i=0; $i<15; $i++){ ?>
+                        <div class="swiper-slide text-center mb-4 hovered-single-author">
+                            <div class="single-author-block d-flex justify-content-center align-items-center p-4">
+                                <div>
+                                    <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/demo-profile.png" alt="">
+                                    <h4 class="ar-bold pt-2">د. رمزي أبو اسماعيل</h4>
+                                    <p class="ar-regular">السياسة / الشرق الأوسط / الحرب</p>
+                                    <a class="mt-3 view-more-btn en-regular" href="#">View Profile</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
         <div class="row py-5 d-lg-flex d-none">
             <div class="col-12">
                 <div class="white-line"></div>
