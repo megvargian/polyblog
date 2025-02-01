38431bed31b921b8bf7cba3a16f8f0e47e98cacd
hrag <hrag.h@hotmail.com>
2025-02-01 12:01:18 +0100

    what we think

--- search.php
+++ search.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying search results pages
  *
@@ -9,26 +10,19 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area col-sm-12 col-lg-8">
-		<main id="main" class="site-main" role="main">
+<section id="primary" class="content-area col-sm-12 col-lg-8">
+	<main id="main" class="site-main" role="main">
 
 		<?php
-		if ( have_posts() ) : ?>
+		if (have_posts()) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'wp-bootstrap-starter' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+				<h1 class="page-title"><?php printf(esc_html__('Search Results for: %s', 'wp-bootstrap-starter'), '<span>' . get_search_query() . '</span>'); ?></h1>
+			</header>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+		<?php
+			while (have_posts()) : the_post();
+				get_template_part('template-parts/content', 'search');
 
 			endwhile;
 
@@ -36,89 +30,91 @@ get_header(); ?>
 
 		else :
 
-			get_template_part( 'template-parts/content', 'none' );
+			get_template_part('template-parts/content', 'none');
 
 		endif; ?>
+	</main>
+</section>
 
-		</main><!-- #main -->
-	</section>
-	<!-- #primary -->
-<?php
-$search_term = isset($_GET['s']) ? sanitize_text_field($_GET['s']) : '';
+<div class="container what-we-think-container">
+	<?php
+	$search_term = isset($_GET['s']) ? sanitize_text_field($_GET['s']) : '';
+	$search_category = isset($_GET['cat']) ? sanitize_text_field($_GET['cat']) : '';
 
-$args = array(
-	'post_type' => 'post',
-	's' => $search_term,
-	'cat' => get_category_by_slug('what-we-think')->term_id,
-);
+	$args = array(
+		'post_type' => 'post',
+		's' => $search_term,
+		'cat' => get_category_by_slug('what-we-think')->term_id,
+	);
 
-$query = new WP_Query($args);
+	$query = new WP_Query($args);
 
-if ($query->have_posts()) : ?>
-	<div class="what-we-think-posts">
-		<div class="row my-4">
-			<?php while ($query->have_posts()) : $query->the_post(); ?>
-				<div class="col-md-6 my-2">
-					<div class="card">
-						<?php
-						$article_thumbnail = get_field('article_thumbnail');
-						if ($article_thumbnail) : ?>
-							<div class="card-img-top">
-								<a href="<?php the_permalink(); ?>">
-									<img class="thumbnail" src="<?php echo esc_url($article_thumbnail); ?>" alt="<?php the_title(); ?>" class="img-fluid">
-								</a>
-							</div>
-						<?php endif; ?>
-						<div class="card-body">
-							<h5 class="card-title align-text-arabic">
-								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-							</h5>
-							<div class="row">
-								<div class="col-4 category-buttons">
-									<div>
-										<?php
-										$categories = get_the_category();
-										foreach ($categories as $category) {
-											if ($category->slug == 'arabic' || $category->slug == 'english') {
-										?>
-												<button type="button" class="<?php echo $category->slug == 'english' ? 'english' : 'arabic'; ?>">
-													<strong><?php echo $category->slug == 'english' ? 'EN' : 'ع'; ?></strong>
-												</button>
-										<?php
-											}
-										}
-										?>
-									</div>
-									<p class="article-date">
-										<?php echo get_the_date('d/m/Y'); ?>
-									</p>
-								</div>
-								<?php
-                                    $author = get_field('author');
-                                ?>
-								<div class="col-5 author-info">
-									<a href="<?php echo get_permalink(get_field('author')); ?>">
-										<h4 class="align-text-arabic"><strong><?php echo get_the_title($author); ?></strong></h4>
+	if ($query->have_posts()) : ?>
+		<div class="what-we-think-posts">
+			<div class="row my-4">
+				<?php while ($query->have_posts()) : $query->the_post(); ?>
+					<div class="col-md-6 my-2">
+						<div class="card">
+							<?php
+							$article_thumbnail = get_field('article_thumbnail');
+							if ($article_thumbnail) : ?>
+								<div class="card-img-top">
+									<a href="<?php the_permalink(); ?>">
+										<img class="thumbnail" src="<?php echo esc_url($article_thumbnail); ?>" alt="<?php the_title(); ?>" class="img-fluid">
 									</a>
 								</div>
-								<div class="col-3">
-									<img class="author-image" src="<?php echo get_the_post_thumbnail_url($author); ?>" alt="<?php echo get_the_title($author); ?>" />
+							<?php endif; ?>
+							<div class="card-body">
+								<h5 class="card-title align-text-arabic">
+									<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+								</h5>
+								<div class="row">
+									<div class="col-4 category-buttons">
+										<div>
+											<?php
+											$categories = get_the_category();
+											foreach ($categories as $category) {
+												if ($category->slug == 'arabic' || $category->slug == 'english') {
+											?>
+													<button type="button" class="<?php echo $category->slug == 'english' ? 'english' : 'arabic'; ?>">
+														<strong><?php echo $category->slug == 'english' ? 'EN' : 'ع'; ?></strong>
+													</button>
+											<?php
+												}
+											}
+											?>
+										</div>
+										<p class="article-date">
+											<?php echo get_the_date('d/m/Y'); ?>
+										</p>
+									</div>
+									<?php
+									$author = get_field('author');
+									?>
+									<div class="col-5 author-info">
+										<a href="<?php echo get_permalink(get_field('author')); ?>">
+											<h4 class="align-text-arabic"><strong><?php echo get_the_title($author); ?></strong></h4>
+										</a>
+									</div>
+									<div class="col-3">
+										<img class="author-image" src="<?php echo get_the_post_thumbnail_url($author); ?>" alt="<?php echo get_the_title($author); ?>" />
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<?php if ($query->current_post % 2 == 1) : ?>
+					<?php if ($query->current_post % 2 == 1) : ?>
+			</div>
+			<div class="row my-4">
+			<?php endif; ?>
+		<?php endwhile; ?>
+			</div>
 		</div>
-		<div class="row my-4">
-		<?php endif; ?>
-	<?php endwhile; ?>
-		</div>
-	</div>
-<?php else : ?>
-	<p>No posts found for this search.</p>
-<?php endif;
-wp_reset_postdata();
-?>
+	<?php else : ?>
+		<p>No posts found for this search.</p>
+	<?php endif;
+	wp_reset_postdata();
+	?>
+</div>
 <?php
 get_footer();
