b7d1be645f4a011ed99cc4efe4e4ba1258b3652b
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-02-03 22:59:53 +0200

    test

--- single-post.php
+++ single-post.php
@@ -26,6 +26,7 @@ $author_posts_args = array(
     'post__not_in' => array($post_id),
 );
 $author_posts_query = new WP_Query($author_posts_args);
+echo apply_filters('wpml_object_id', $post_id, 'post', true, do_shortcode('[language]')); // Get original ID in English
 $translations = apply_filters('wpml_get_element_translations', null, $post_id, 'post');
 if (have_posts()):
     while (have_posts()):
