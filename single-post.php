fa9d90c273a520e46f9c96c56f4f1ee384861213
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-02-06 16:44:09 +0200

    tesT

--- single-post.php
+++ single-post.php
@@ -26,7 +26,10 @@ $tags = get_the_tags();
 //     'post__not_in' => array($post_id),
 // );
 // $author_posts_query = new WP_Query($author_posts_args);
-$original_post_id = apply_filters('wpml_object_id', $post_id, 'post', true);
+$current_language = apply_filters('wpml_current_language', null);
+$second_lang_post= apply_filters('wpml_object_id', $post_id, 'post', true, $current_language === 'ar' ? 'en' : 'ar');
+echo $second_lang_post;
+echo $current_language;
 $translations = apply_filters('wpml_get_element_translations', null, $post_id, 'post');
 if (have_posts()):
     while (have_posts()):
