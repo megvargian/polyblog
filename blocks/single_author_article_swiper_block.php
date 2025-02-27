284bdaf5d22b2c08a7028398707037e65297473a
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-02-27 18:17:08 +0200

    test

--- blocks/single_author_article_swiper_block.php
+++ blocks/single_author_article_swiper_block.php
@@ -6,40 +6,41 @@
 $single_author_article_swiper_block = get_fields();
 $all_articles = $single_author_article_swiper_block['articles'];
 ?>
+<pre><?php print_r($all_articles);?></pre>
 <section class="py-5 single-author-article-swiper-section">
     <div class="container position-relative">
         <div class="swiper single-author-article-swiper">
             <div class="swiper-wrapper">
-                <?php foreach ($all_articles as $key => $article) {
-                        $article_id = $article['article'];
-                        $article_title = get_the_title($article_id);
-                        $get_article_fields = get_fields($article_id);
-                        $author_id = $get_article_fields['author'];
-                        $featured_image = $get_article_fields['article_thumbnail'];
-                        $author_image = get_the_post_thumbnail_url($author_id);
-                        $author_title = get_the_title($author_id);
-                        $author_excpert = get_the_excerpt($author_id);
+                <!-- <?php //foreach ($all_articles as $key => $article) {
+                        // $article_id = $article['article'];
+                        // $article_title = get_the_title($article_id);
+                        // $get_article_fields = get_fields($article_id);
+                        // $author_id = $get_article_fields['author'];
+                        // $featured_image = $get_article_fields['article_thumbnail'];
+                        // $author_image = get_the_post_thumbnail_url($author_id);
+                        // $author_title = get_the_title($author_id);
+                        // $author_excpert = get_the_excerpt($author_id);
 
                     ?>
                     <div class="swiper-slide single-red-border">
                         <div class="row">
                             <div class="col-6 custom-padding-right">
-                                <img class="w-100 h-100 d-block single-red-border-right" src="<?php echo $featured_image; ?>" alt="<?php echo $article_title; ?>">
+                                <img class="w-100 h-100 d-block single-red-border-right" src="<?php //echo $featured_image; ?>" alt="<?php //echo $article_title; ?>">
                             </div>
                             <div class="col-6 custom-padding-left">
                                 <div class="d-flex justify-content-center align-items-center px-sm-5 p-2 h-100">
                                     <div class="inner-author mx-auto">
                                         <p class="text-left d-md-block d-none">
-                                            <?php echo $author_excpert; ?>
+                                            <?php //echo $author_excpert; ?>
                                         </p>
-                                        <img class="single-author-img d-flex mx-auto" src="<?php echo $author_image; ?>" alt="<?php echo $author_title; ?>">
-                                        <h6 class="pt-3 text-center"><?php echo $author_title; ?></h6>
+                                        <img class="single-author-img d-flex mx-auto" src="<?php //echo $author_image; ?>" alt="<?php //echo $author_title; ?>">
+                                        <h6 class="pt-3 text-center"><?php //echo $author_title; ?></h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                <? } ?>
+                <? //} ?> -->
                 <!-- <div class="swiper-slide single-red-border">
                     <div class="row">
                         <div class="col-6 custom-padding-right">
