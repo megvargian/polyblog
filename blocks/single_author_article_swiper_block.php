fe218e91e602d70fb89de148ca51c028fcf32160
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-02-27 18:51:03 +0200

    test

--- blocks/single_author_article_swiper_block.php
+++ blocks/single_author_article_swiper_block.php
@@ -40,60 +40,6 @@ $all_articles = $single_author_article_swiper_block['articles'];
                         </div>
                     </div>
                 <?php } ?>
-                <!-- <div class="swiper-slide single-red-border">
-                    <div class="row">
-                        <div class="col-6 custom-padding-right">
-                            <img class="w-100 h-100 d-block single-red-border-right" src="<?php //echo get_template_directory_uri(); ?>/inc/assets/images/single-article-img.png" alt="article-title">
-                        </div>
-                        <div class="col-6 custom-padding-left">
-                            <div class="d-flex justify-content-center align-items-center px-sm-5 p-2 h-100">
-                                <div class="inner-author mx-auto">
-                                    <p class="text-left d-md-block d-none">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam doloremque exercitationem qui laboriosam quasi ducimus fuga atque tenetur maiores quaerat, suscipit, vel similique porro, officiis harum pariatur sapiente. Nihil, deleniti?
-                                    </p>
-                                    <img class="single-author-img d-flex mx-auto" src="<?php //echo get_template_directory_uri(); ?>/inc/assets/images/profile.png" alt="">
-                                    <h6 class="pt-3 text-center">Name of the Author</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide single-red-border">
-                    <div class="row">
-                        <div class="col-6">
-                            <img class="w-100 h-100 d-block single-red-border-right" src="<?php //echo get_template_directory_uri(); ?>/inc/assets/images/single-article-img.png" alt="article-title">
-                        </div>
-                        <div class="col-6 custom-padding-left">
-                            <div class="d-flex justify-content-center align-items-center px-sm-5 p-2 h-100">
-                                <div class="inner-author mx-auto">
-                                    <p class="text-left d-md-block d-none">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam doloremque exercitationem qui laboriosam quasi ducimus fuga atque tenetur maiores quaerat, suscipit, vel similique porro, officiis harum pariatur sapiente. Nihil, deleniti?
-                                    </p>
-                                    <img class="single-author-img d-flex mx-auto" src="<?php //echo get_template_directory_uri(); ?>/inc/assets/images/profile.png" alt="">
-                                    <h6 class="pt-3 text-center">Name of the Author</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide single-red-border">
-                    <div class="row">
-                        <div class="col-6">
-                            <img class="w-100 h-100 d-block single-red-border-right" src="<?php //echo get_template_directory_uri(); ?>/inc/assets/images/single-article-img.png" alt="article-title">
-                        </div>
-                        <div class="col-6 custom-padding-left">
-                            <div class="d-flex justify-content-center align-items-center px-sm-5 p-2 h-100">
-                                <div class="inner-author mx-auto">
-                                    <p class="text-left d-md-block d-none">
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam doloremque exercitationem qui laboriosam quasi ducimus fuga atque tenetur maiores quaerat, suscipit, vel similique porro, officiis harum pariatur sapiente. Nihil, deleniti?
-                                    </p>
-                                    <img class="single-author-img d-flex mx-auto" src="<?php //echo get_template_directory_uri(); ?>/inc/assets/images/profile.png" alt="">
-                                    <h6 class="pt-3 text-center">Name of the Author</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
             </div>
             <?php
                 if(!isMob()){
