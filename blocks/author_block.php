b1d793b9e95df9b578a63984957ba54cf0f5abf2
hrag <hrag.h@hotmail.com>
2025-03-01 17:42:43 +0100

    author block popup

--- blocks/author_block.php
+++ blocks/author_block.php
@@ -18,7 +18,7 @@ $rows = array_chunk($authors_list, 4);
                     $author_expertise = $author['sub_title'];
                     ?>
                     <div class="col-3 single-author-col">
-                        <a href="#" target="_blank">
+                        <a href="#">
                             <div class="single-author-container">
                                 <div class="author-image">
                                     <img class="img-fluid" src="<?php echo $author['profile_image'] ?>"
@@ -39,16 +39,14 @@ $rows = array_chunk($authors_list, 4);
             <?php foreach ($rows as $row) { ?>
                 <?php foreach ($row as $author) { ?>
                     <?php
-                    $author_id = $author['author'];
-                    $author_title = get_the_title($author_id);
-                    $author_expertise = get_field('expertise', $author_id);
-                    $author_link = get_permalink($author_id);
+                     $author_title = $author['title'];
+                     $author_expertise = $author['sub_title'];
                     ?>
                     <div class="col-4 single-author-col">
-                        <a href="<?php echo $author_link; ?>" target="_blank">
+                        <a href="#">
                             <div class="single-author-container">
                                 <div class="author-image">
-                                    <img class="img-fluid" src="<?php echo get_the_post_thumbnail_url($author_id); ?>"
+                                    <img class="img-fluid" src="<?php echo $author['profile_image'] ?>"
                                         alt="<?php echo $author_title ?>" />
                                 </div>
                             </div>
