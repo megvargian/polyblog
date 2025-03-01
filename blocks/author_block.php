e1c3e7b698e282d9b1b99f1f9dc17031b96f3fc8
hrag <hrag.h@hotmail.com>
2025-03-01 17:40:14 +0100

    author block popup

--- blocks/author_block.php
+++ blocks/author_block.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * HomePage Author Block Template
  */
@@ -13,16 +14,14 @@ $rows = array_chunk($authors_list, 4);
             <div class="row my-3 justify-content-center">
                 <?php foreach ($row as $author) { ?>
                     <?php
-                    $author_id = $author['author'];
-                    $author_title = get_the_title($author_id);
-                    $author_expertise = get_field('expertise', $author_id);
-                    $author_link = get_permalink($author_id);
+                    $author_title = $author['title'];
+                    $author_expertise = $author['sub_title'];
                     ?>
                     <div class="col-3 single-author-col">
-                        <a href="<?php echo $author_link; ?>" target="_blank">
+                        <a href="#" target="_blank">
                             <div class="single-author-container">
                                 <div class="author-image">
-                                    <img class="img-fluid" src="<?php echo get_the_post_thumbnail_url($author_id); ?>"
+                                    <img class="img-fluid" src="<?php echo $author['profile_image'] ?>"
                                         alt="<?php echo $author_title ?>" />
                                 </div>
                                 <p><?php echo esc_html($author_title); ?></p>
@@ -34,21 +33,8 @@ $rows = array_chunk($authors_list, 4);
             </div>
         <?php } ?>
     </div>
+    <!-- VVV mobile VVV -->
     <div class="container d-md-none d-block">
-        <div class="row single-red-border mx-1">
-            <div class="col-6">
-                <div class="d-flex justify-content-center align-items-center px-sm-5 px-2 h-100">
-                    <div class="inner-author mx-auto">
-                        <p class="text-center" style="color: #fff;">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam doloremque exercitationem qui
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 px-0">
-                <img class="w-100 h-100 d-block single-red-border-left" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/board-img-mobile.png" alt="article-title">
-            </div>
-        </div>
         <div class="row my-3 justify-content-center">
             <?php foreach ($rows as $row) { ?>
                 <?php foreach ($row as $author) { ?>
@@ -74,6 +60,5 @@ $rows = array_chunk($authors_list, 4);
     </div>
 </section>
 <script>
-    jQuery(document).ready(function ($) {
-    });
+    jQuery(document).ready(function($) {});
 </script>
