081b564ab7fd8e7e51668dd023e16fb154ceedab
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-03-01 16:31:53 +0200

    test

--- blocks/yt_featured_vids_block.php
+++ blocks/yt_featured_vids_block.php
@@ -39,7 +39,35 @@ $yt_featured_vids_block = get_fields();
     <div class="container-fluid d-lg-none d-block py-4 position-relative">
         <div class="swiper youtube-production-slider">
             <div class="swiper-wrapper">
-                <div class="swiper-slide">
+                <?php foreach ($yt_featured_vids_block['mobile_section']['youtube_slides'] as $key => $yt_slide) {
+                    if($yt_slide['youtube_video_or_view_more_section']){?>
+                    <div class="swiper-slide">
+                        <div class="youtube-video-link">
+                            <img class="youtube-playlist-btn"
+                                src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/youtube-production-icon.svg"
+                                alt="YouTube">
+                        </div>
+                    </div>
+                 <?php } else { ?>
+                    <div class="swiper-slide">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <div class="d-block">
+                                <div class="youtube-full-circle"></div>
+                                <a href="#" class="view-more-btn">
+                                    View more
+                                </a>
+                            </div>
+                            <div class="d-block mx-3">
+                                <p>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia at molestias,
+                                    perspiciatis voluptatem libero deleniti facilis quod laborum in, labore obcaecati
+                                    minima, magni dicta ad alias nisi veniam consequatur dolore?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                <?php }} ?>
+                <!-- <div class="swiper-slide">
                     <div class="youtube-video-link">
                         <img class="youtube-playlist-btn"
                             src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/youtube-production-icon.svg"
@@ -110,7 +138,7 @@ $yt_featured_vids_block = get_fields();
                             </p>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
         <div class="swiper-pagination-youtube-production-slider"></div>
