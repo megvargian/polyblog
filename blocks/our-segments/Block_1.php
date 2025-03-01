982c7e1ba13416e69342bbfc7296654ddbe2c0ae
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-03-01 15:27:29 +0200

    add fields

--- blocks/our-segments/Block_1.php
+++ blocks/our-segments/Block_1.php
@@ -9,19 +9,19 @@ $block_one_fields = get_fields();
     <div class="container">
         <div class="row pb-5">
             <div class="col-12 pb-5">
-                <img class="d-md-block d-none w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/our-segements-first-img.png" alt="">
-                <img class="d-md-none d-block w-100" src="<?php echo get_template_directory_uri(); ?>/inc/assets/images/our-segements-mob.png" alt="">
+                <img class="d-md-block d-none w-100" src="<?php echo $block_one_fields['desktop_image']; ?>" alt="desktop_image">
+                <img class="d-md-none d-block w-100" src="<?php echo $block_one_fields['mobile_image']; ?>" alt="mobile_image">
             </div>
             <div class="col-6 text-left">
-                <h5 class="en-bold mb-2">SYNOPSIS</h5>
+                <h5 class="en-bold mb-2"><?php echo $block_one_fields['en_title']; ?></h5>
                 <p class="en-regular">
-                    In Syria, following the recent ousting of President Bashar al-Assad,In Syria, following the recent ousting of President Bashar al-Assad,In Syria
+                    <?php echo $block_one_fields['en_paragraph']; ?>
                 </p>
             </div>
             <div class="col-6 text-right">
-                <h5 class="ar-bold mb-2">ملخص</h5>
+                <h5 class="ar-bold mb-2"><?php echo $block_one_fields['ar_title']; ?></h5>
                 <p class="ar-regular">
-                لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار لكن لا بد أن أوضح لك أن كل هذه الأفكار المغلوطة حول لكن لا بد أن أوضح لك أن كل هذه الأفكار
+                    <?php echo $block_one_fields['ar_paragraph']; ?>
                 </p>
             </div>
         </div>
