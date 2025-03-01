6c0acfb7711b8df17c2fa2597cdac85292d45234
hrag <hrag.h@hotmail.com>
2025-03-01 17:53:45 +0100

    author block popup

--- blocks/author_block.php
+++ blocks/author_block.php
@@ -18,7 +18,8 @@ $rows = array_chunk($authors_list, 4);
                     $author_expertise = $author['sub_title'];
                     ?>
                     <div class="col-3 single-author-col">
-                        <a href="#">
+                        <a data-bs-toggle="modal"
+                            data-bs-target="#<?php echo "modal-" . strtolower(str_replace(' ', '-', $author_title)); ?>">
                             <div class="single-author-container">
                                 <div class="author-image">
                                     <img class="img-fluid" src="<?php echo $author['profile_image'] ?>"
@@ -39,11 +40,12 @@ $rows = array_chunk($authors_list, 4);
             <?php foreach ($rows as $row) { ?>
                 <?php foreach ($row as $author) { ?>
                     <?php
-                     $author_title = $author['title'];
-                     $author_expertise = $author['sub_title'];
+                    $author_title = $author['title'];
+                    $author_expertise = $author['sub_title'];
                     ?>
                     <div class="col-4 single-author-col">
-                        <a href="#">
+                        <a data-bs-toggle="modal"
+                            data-bs-target="#<?php echo "modal-" . strtolower(str_replace(' ', '-', $author_title)); ?>">
                             <div class="single-author-container">
                                 <div class="author-image">
                                     <img class="img-fluid" src="<?php echo $author['profile_image'] ?>"
@@ -56,7 +58,24 @@ $rows = array_chunk($authors_list, 4);
             <?php } ?>
         </div>
     </div>
+    <?php foreach ($rows as $row) { ?>
+        <?php foreach ($row as $author) { ?>
+            <div class="modal fade custom-modal"
+                id="<?php echo "modal-" . strtolower(str_replace(' ', '-', $author['title'])); ?>" tabindex="-1"
+                aria-labelledby="<?php echo "modal-" . strtolower(str_replace(' ', '-', $author['title'])); ?>Label"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg">
+                    <div class="modal-content border-0">
+                        <div class="modal-header">
+                            <button type="button" class="btn-close m-0 remove-border-onFocus" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <?php echo $author['paragraph']; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    <?php } ?>
 </section>
-<script>
-    jQuery(document).ready(function($) {});
-</script>
