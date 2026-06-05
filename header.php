c56f569e64b393ea40db46c808694719145bafa9
Megvargian123 <mikekouyoumdjian@hotmail.com>
2026-06-05 17:35:04 +0300

    add google analytics

--- header.php
+++ header.php
@@ -15,6 +15,15 @@ $header_fields = get_fields('options');
 <html <?php language_attributes(); ?>>
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2YNP450RWN"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-2YNP450RWN');
+    </script>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
