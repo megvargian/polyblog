1896db7a2e4f0c47d32c4f512223f72280543c96
Megvargian123 <mikekouyoumdjian@hotmail.com>
2026-06-12 19:44:21 +0300

    change font

---
+++ get-to-know-us-page.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Get to Know Us Page
+ */
+
+get_header();
+$page_fields = get_fields();
+?>
+<section class="get-to-know-us-page">
+    <div class="container">
+        <div class="row pt-2 mb-5 d-lg-flex d-none">
+            <div class="col-12">
+                <div class="white-line"></div>
+            </div>
+        </div>
+        <div class="row text-center py-5">
+            <div class="col-12">
+                <div class="bg-black">
+                    <h1>
+                        <span class="en-bold"><?php the_title(); ?></span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    while ( have_posts() ) : the_post();
+        the_content();
+    endwhile;
+    ?>
+</section>
+<?php
+get_footer();
