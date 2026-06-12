1179edc3850885e4f7886d59e9ddab09dfc9fb03
Megvargian123 <mikekouyoumdjian@hotmail.com>
2026-06-12 23:02:16 +0300

    change font

--- our-production-page.php
+++ our-production-page.php
@@ -22,17 +22,11 @@ get_header();
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <?php
-                    while ( have_posts() ) : the_post();
-                        the_content();
-                    endwhile;
-                ?>
-            </div>
-        </div>
-    </div>
+    <?php
+        while ( have_posts() ) : the_post();
+            the_content();
+        endwhile;
+    ?>
 </section>
 <?php
 get_footer();
