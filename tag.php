689e03cf75182ea563096edbf89e21340281d616
Megvargian123 <mikekouyoumdjian@hotmail.com>
2026-06-16 20:31:49 +0300

    fix play button on tags

--- tag.php
+++ tag.php
@@ -72,14 +72,7 @@ endif;
                             <a class="<?php echo $is_youtube_video ? 'w-100 h-100 position-relative' : ''; ?> " href="<?php echo $is_youtube_video ? esc_url($is_youtube_video) : esc_url($article_link); ?>">
                                 <?php if($is_youtube_video) : ?>
                                     <svg id="Layer_1" class="play-icon position-absolute" alt="play" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 449.9 500">
-                                        <defs>
-                                            <style>
-                                            .st0 {
-                                                fill: #fff;
-                                            }
-                                            </style>
-                                        </defs>
-                                        <path class="st0" d="M81.5,11.05C36.54-14.83,0,6.32,0,58.23v382.88c0,51.93,36.54,73.05,81.5,47.26l334.67-191.96c44.98-25.79,44.98-67.68,0-93.49L81.5,11.05ZM81.5,11.05"/>
+                                        <path class="st0" fill="#fff" d="M81.5,11.05C36.54-14.83,0,6.32,0,58.23v382.88c0,51.93,36.54,73.05,81.5,47.26l334.67-191.96c44.98-25.79,44.98-67.68,0-93.49L81.5,11.05ZM81.5,11.05"/>
                                     </svg>
                                 <?php endif; ?>
                                 <img class="d-block w-100" src="<?php echo esc_url($get_image); ?>"
