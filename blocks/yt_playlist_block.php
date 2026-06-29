10e9fc0bc3c39f3c6ec08e15362ac39da91a44a8
Megvargian123 <mikekouyoumdjian@hotmail.com>
2026-06-29 17:21:56 +0300

    fix auto play and remove icon

--- blocks/yt_playlist_block.php
+++ blocks/yt_playlist_block.php
@@ -22,17 +22,17 @@ $row = $yt_playlist_block['one_row_youtube_section'];
             </div>
         </div>
         <div class="row custom-min-height mb-3">
-            <video class="video w-100" width="100%" autoplay loop muted>
+            <video class="video w-100" width="100%" autoplay muted loop playsinline webkit-playsinline preload="auto">
                 <source src="<?php echo esc_url($yt_playlist_block['video']); ?>" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
-            <div class="col d-flex justify-content-center align-items-center yt-icon">
-                <a href="<?php echo $yt_playlist_block['youtube_link']; ?>">
+            <!-- <div class="col d-flex justify-content-center align-items-center yt-icon">
+                <a href="<?php //echo $yt_playlist_block['youtube_link']; ?>">
                     <img class="youtube-playlist-btn d-block"
-                        src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/youtube-production-icon.svg"
+                        src="<?php //echo get_template_directory_uri(); ?>/inc/assets/icons/youtube-production-icon.svg"
                         alt="YouTube">
                 </a>
-            </div>
+            </div> -->
         </div>
         <div class="row justify-content-between align-items-center">
             <div class="col">
@@ -50,7 +50,8 @@ $row = $yt_playlist_block['one_row_youtube_section'];
     <div class="container-fluid d-lg-block d-none">
         <div class="row justify-content-center">
             <div class="col youtube-playlist-button-col justify-content-center d-flex align-items-center">
-                <a class="justify-content-center d-flex align-items-center" href="<?php echo $row['main_link'] ?>" target="_blank">
+                <a class="justify-content-center d-flex align-items-center" href="<?php echo $row['main_link'] ?>"
+                    target="_blank">
                     <div class="d-block">
                         <p class="en-bold text-center"><?php echo $row['en_title'];?></p>
                         <p class="ar-bold text-center"><?php echo $row['ar_title'];?></p>
@@ -58,7 +59,8 @@ $row = $yt_playlist_block['one_row_youtube_section'];
                 </a>
             </div>
             <div class="col youtube-playlist-button-col justify-content-center d-flex align-items-center">
-                <a class="justify-content-center d-flex align-items-center" href="<?php echo $row['main_link'] ?>" target="_blank">
+                <a class="justify-content-center d-flex align-items-center" href="<?php echo $row['main_link'] ?>"
+                    target="_blank">
                     <img class="youtube-playlist-btn d-block"
                         src="<?php echo get_template_directory_uri(); ?>/inc/assets/icons/youtube-production-icon.svg"
                         alt="YouTube">
@@ -66,11 +68,13 @@ $row = $yt_playlist_block['one_row_youtube_section'];
             </div>
             <div class="col youtube-playlist-button-col justify-content-center d-flex align-items-center">
                 <a class="justify-content-center d-flex align-items-center" href="<?php echo $row['main_link'] ?>">
-                    <img class="youtube_featured_image d-block" src="<?php echo $row['featured_image']; ?>" alt="YouTube">
+                    <img class="youtube_featured_image d-block" src="<?php echo $row['featured_image']; ?>"
+                        alt="YouTube">
                 </a>
             </div>
             <div class="col youtube-playlist-button-col justify-content-center d-flex align-items-center">
-                <a class="justify-content-center d-flex align-items-center" href="<?php echo $row['main_link'] ?>" target="_blank">
+                <a class="justify-content-center d-flex align-items-center" href="<?php echo $row['main_link'] ?>"
+                    target="_blank">
                     <div class="bg-black d-block">
                         <p class="en-regular"><?php echo $row['en_title_episode'] ?></p>
                         <p class="ar-regular"><?php echo $row['ar_title_episode'] ?></p>
