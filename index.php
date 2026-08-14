fab8d63206d7ad46ed306f5b979c23eb059c3100
Megvargian123 <mikekouyoumdjian@hotmail.com>
2026-08-14 10:22:06 +0300

    add video on mobile only

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
  */
 
 get_header();
+$header_fields = get_fields('option');
+?>
+<div class="video-container d-block d-md-none">
+    <video class="video" width="100%" autoplay loop muted>
+        <source src="<?php echo esc_url($header_fields['header_video']); ?>" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+</div>
+<?php
 while ( have_posts() ) : the_post();
     the_content();
 endwhile;
